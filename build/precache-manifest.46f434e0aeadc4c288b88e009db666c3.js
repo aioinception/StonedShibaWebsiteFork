8e123024f2603608c50d9f712baf2a8d35d6a12b
Marcelo Resola <81779796+ResolaMarcelo@users.noreply.github.com>
2022-10-28 18:48:36 -0300

    Add files via upload

---
+++ build/precache-manifest.46f434e0aeadc4c288b88e009db666c3.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = [
+  {
+    "revision": "97b831b19815770a5cb9",
+    "url": "/static/css/main.c94c5429.chunk.css"
+  },
+  {
+    "revision": "97b831b19815770a5cb9",
+    "url": "/static/js/main.97b831b1.chunk.js"
+  },
+  {
+    "revision": "5bd9b8396460e3b03820",
+    "url": "/static/js/1.5bd9b839.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e9d3df723bea77130722a899c634d8a9",
+    "url": "/static/media/nft1.e9d3df72.png"
+  },
+  {
+    "revision": "883fdb52c3f13ca8479ccc94ef00ae79",
+    "url": "/static/media/nft2.883fdb52.png"
+  },
+  {
+    "revision": "b23452cac050be340ee9531c1b0aaeb0",
+    "url": "/static/media/nft3.b23452ca.png"
+  },
+  {
+    "revision": "d670fa7c7667687108b0dfb5546e85b9",
+    "url": "/static/media/nft4.d670fa7c.png"
+  },
+  {
+    "revision": "2056b5a4219f529bc73993089fa53ab1",
+    "url": "/static/media/nft5.2056b5a4.png"
+  },
+  {
+    "revision": "b20c97fea5fc9b64dfb866d59a36912d",
+    "url": "/static/media/nft6.b20c97fe.png"
+  },
+  {
+    "revision": "dcedf19052e130976233c0f66e360b12",
+    "url": "/static/media/nft7.dcedf190.png"
+  },
+  {
+    "revision": "188adc59608c09451a091be4c1ae5c62",
+    "url": "/static/media/nft8.188adc59.png"
+  },
+  {
+    "revision": "bedb88fb3e910d67a127d7a107732c72",
+    "url": "/static/media/smoke-video.bedb88fb.mp4"
+  },
+  {
+    "revision": "6af6869e0ded952f57b9a2cb9bbc7e21",
+    "url": "/static/media/shiba-logo.6af6869e.png"
+  },
+  {
+    "revision": "4e1f14f01c56147c0f59381aaabfb338",
+    "url": "/static/media/helvari.4e1f14f0.ttf"
+  },
+  {
+    "revision": "ad1ba83641962ffe080388bf07d646ab",
+    "url": "/static/media/Beams.ad1ba836.ttf"
+  },
+  {
+    "revision": "76e872bc911c3d908aeaf31b2c16bc63",
+    "url": "/static/media/Inter-Regular.76e872bc.otf"
+  },
+  {
+    "revision": "d759e235e88e47f838062c7ab97308b1",
+    "url": "/static/media/Inter-Bold.d759e235.otf"
+  },
+  {
+    "revision": "e6fef702b507237e0033f4244cc4389c",
+    "url": "/static/media/Inter-Black.e6fef702.otf"
+  },
+  {
+    "revision": "d7019947105844db1899d246172f06b4",
+    "url": "/static/media/Inter-Light.d7019947.otf"
+  },
+  {
+    "revision": "72869267880104b27bed47fdf7e5c75d",
+    "url": "/static/media/Inter-Thin.72869267.otf"
+  },
+  {
+    "revision": "75ebeb1ce6c32f8e4a8e513142da54c5",
+    "url": "/static/media/pruebaexport229.75ebeb1c.jfif"
+  },
+  {
+    "revision": "10cbce6999222177ae5435fe0463df9f",
+    "url": "/static/media/Kush_Coin.10cbce69.png"
+  },
+  {
+    "revision": "d5de663848e60f23993ed36653505f21",
+    "url": "/static/media/nft9.d5de6638.png"
+  },
+  {
+    "revision": "da3f01d9d46619951da16750f1b083df",
+    "url": "/static/media/nft10.da3f01d9.jpg"
+  },
+  {
+    "revision": "3637cf66dec0d585b19fe8be72b58da6",
+    "url": "/static/media/inception-logo.3637cf66.png"
+  },
+  {
+    "revision": "448eede28337eb71dd895546638209e0",
+    "url": "/index.html"
+  }
+];
